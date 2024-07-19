e05c14bcab0eebbf6b965aa902617c1e459fad7a
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-07-19 21:41:44 +0500

    fixed issues

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,10 +1,9 @@
 /* eslint-disable camelcase */
-import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-
+import { clerkClient } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
